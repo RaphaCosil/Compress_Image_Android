754b75baf6560c31552df0ac4cc5d2be14c2774a
RaphaCosil <Raphael.Cosil@gmail.com>
2024-06-12 15:56:38 -0300

    ADD - Deixando o código mais limpo e outras coisas

--- app/src/main/java/com/example/compressimagestudy/ZeloryCompressor.kt
+++ app/src/main/java/com/example/compressimagestudy/ZeloryCompressor.kt
@@ -2,6 +2,7 @@ package com.example.compressimagestudy
 import android.content.Context
 import id.zelory.compressor.Compressor
 import id.zelory.compressor.constraint.quality
+import id.zelory.compressor.constraint.size
 import java.io.File
 class ZeloryCompressor {
     suspend fun compressFile(context: Context, file: File, imageQuality: Int): File {
